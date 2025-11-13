7dd38b0faa3803c9481f5bc979e6aa43cfe806ba
klarahuss <karaso-5@student.ltu.se>
2025-11-13 11:12:23 +0000

    new branch

--- dragongame/src/main/java/dragongame/Main.java
+++ dragongame/src/main/java/dragongame/Main.java
@@ -3,7 +3,7 @@ package dragongame;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!");
-        System.out.println("hello Malin!");
-        
+        System.out.println("hello!");
+
     }
 }
