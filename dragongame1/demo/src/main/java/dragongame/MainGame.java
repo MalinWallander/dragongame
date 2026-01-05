47285a82cdb3bd0bac62d5d0fb0bc7e7e064dc1c
klarahuss <karaso-5@student.ltu.se>
2026-01-05 17:33:30 +0100

    cleanup

--- dragongame1/demo/src/main/java/dragongame/MainGame.java
+++
@@ -1,9 +0,0 @@
-package dragongame;
-
-public class MainGame {
-	// Huvudmetod som startar spelet
-	public static void main(String[] args) {
-		GameEngine game = new GameEngine();
-		game.playGame();
-	}
-}
